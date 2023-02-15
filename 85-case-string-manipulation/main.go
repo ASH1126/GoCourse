070905fa29e945e0420b7ac5c4e513c1dd2316f1
Anita Sri Handayani <anita.handa26@gmail.com>
2023-02-15 07:44:15 +0700

    strings

---
+++ 85-case-string-manipulation/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	myString := "This is a clear EXAMPLE of why we search in one case only."
+
+	searchString := strings.ToLower(myString)
+
+	if strings.Contains(searchString, "this") {
+		fmt.Println("Found it!")
+	} else {
+		fmt.Println("Did not find it!")
+	}
+
+	// other case functions
+	fmt.Println(strings.ToLower(myString))
+	fmt.Println(strings.ToUpper(myString))
+	fmt.Println(strings.Title(myString))
+}
+
+
